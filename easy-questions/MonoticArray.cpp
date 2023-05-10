8a202f8f01c5a761da3d10ea4eea48ebf0c9ce1d
MasonManca <mason.manca11@gmail.com>
2023-05-10 13:47:24 -0700

    starting monotic array problem

---
+++ easy-questions/MonoticArray.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+
+using namespace std; 
+
+
+// An array is monotonic if it is either monotone increasing or monotone decreasing.
+// An array nums is monotone increasing if for all i <= j, nums[i] <= nums[j] (Array[i] =< Array[i+1]).
+// An array nums is monotone decreasing if for all i <= j, nums[i] >= nums[j](Array[i] >= Array[i+1]).
+// Given an integer array nums, return true if the given array is monotonic, or false otherwise.
+
+class Solution {
+
+    public: 
+    
+    bool isMonotonic(vector<int>& nums){
+
+
+    }
+};
